db6a9ff106ae32e09fbc1d05df75f0ef5bc4ed65
Jon Lauridsen <mail@jonlauridsen.com>
2022-11-06 21:35:22 +0100

    Export `GithubMockCache` via github/mod.ts

--- github/mod.ts
+++ github/mod.ts
@@ -1,4 +1,4 @@
 export * from "./types.ts";
-export { GithubDiskCache } from "./github-cache.ts";
+export { GithubDiskCache, GithubMockCache } from "./github-cache.ts";
 export { ReadonlyGithubClient, SyncableGithubClient } from "./github-client.ts";
 export { parseGithubUrl } from "./parse-github-url.ts";
