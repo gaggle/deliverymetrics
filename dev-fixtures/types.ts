701c039d16acd56a9beaceff2a206ae5ed49e647
Jon Lauridsen <mail@jonlauridsen.com>
2022-12-10 22:42:07 +0100

    Extract fetchSpecCommand schema + type

--- dev-fixtures/types.ts
+++ dev-fixtures/types.ts
@@ -1,12 +1,16 @@
 import * as z from "zod";
 
+export const fetchSpecCommand = z.object({
+  url: z.string(),
+  name: z.string().optional(),
+  method: z.enum(["GET", "POST"]).optional(),
+  body: z.object({}).catchall(z.string()).optional(),
+  json: z.boolean().optional(),
+});
+export type FetchSpecCommand = z.infer<typeof fetchSpecCommand>;
+
 export const fetchSpecSchema = z.union([
   z.string(),
-  z.object({
-    url: z.string(),
-    name: z.string().optional(),
-    method: z.enum(["GET", "POST"]).optional(),
-    body: z.object({}).catchall(z.string()).optional(),
-  }),
+  fetchSpecCommand,
 ]).array();
 export type FetchSpec = z.infer<typeof fetchSpecSchema>;
