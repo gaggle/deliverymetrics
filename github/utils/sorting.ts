e22da2694d16018cb57d97f04e38aca5c84ab0e0
Jon Lauridsen <mail@jonlauridsen.com>
2022-12-13 01:36:27 +0100

    Add sortActionRunsKey function

--- github/utils/sorting.ts
+++ github/utils/sorting.ts
@@ -1,4 +1,4 @@
-import { GithubPull, GithubPullDateKey } from "../types/mod.ts";
+import { ActionRun, GithubPull, GithubPullDateKey } from "../types/mod.ts";
 
 export function sortPullsByKey(
   pulls: Array<GithubPull>,
@@ -14,6 +14,24 @@ export function sortPullsByKey(
     if (aT > bT) return 1;
     throw new Error(
       `Error sorting pulls ${a.number} (${aT}) and ${b.number} (${bT})`,
+    );
+  });
+}
+
+export function sortActionRunsKey(
+  items: Array<ActionRun>,
+  key: "created_at" | "updated_at" = "updated_at",
+): Array<ActionRun> {
+  return items.sort((a, b) => {
+    const aVal = a[key];
+    const aT = aVal === null ? 0 : new Date(aVal).getTime();
+    const bVal = b[key];
+    const bT = bVal === null ? 0 : new Date(bVal).getTime();
+    if (aT === bT) return 0;
+    if (aT < bT) return -1;
+    if (aT > bT) return 1;
+    throw new Error(
+      `Error sorting ${aT} and ${bT}`,
     );
   });
 }
