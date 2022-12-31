dbc9e69964cc42a164b7f176a7dbc2ca79b7683d
Jon Lauridsen <mail@jonlauridsen.com>
2022-12-31 22:19:22 +0100

    Lazy-load AloeDatabase
    This way they don't take time to load (but they do block when used for the first time)

--- db/aloe-database.ts
+++ db/aloe-database.ts
@@ -9,6 +9,7 @@ type DatabaseDocument = Document & { _id?: never };
 
 class BaseAloeDatabase<Schema extends DatabaseDocument> {
   protected db: Database<Acceptable<Schema>>;
+  protected isLoaded = false;
 
   protected constructor(
     { path: fp, schema }: {
@@ -19,54 +20,70 @@ class BaseAloeDatabase<Schema extends DatabaseDocument> {
     this.db = new Database<Acceptable<Schema>>({
       path: fp,
       validator: schema.parse,
+      autoload: false,
     });
   }
 
-  count(
+  protected async ensureLoaded(): Promise<void> {
+    if (this.isLoaded === false) {
+      await this.db.load();
+      this.isLoaded = true;
+    }
+  }
+
+  async count(
     ...args: Parameters<Database<Acceptable<Schema>>["count"]>
   ): ReturnType<Database<Acceptable<Schema>>["count"]> {
+    await this.ensureLoaded();
     return this.db.count(...args);
   }
 
-  findOne(
+  async findOne(
     ...args: Parameters<Database<Acceptable<Schema>>["findOne"]>
   ): ReturnType<Database<Acceptable<Schema>>["findOne"]> {
+    await this.ensureLoaded();
     return this.db.findOne(...args);
   }
 
-  findMany(
+  async findMany(
     ...args: Parameters<Database<Acceptable<Schema>>["findMany"]>
   ): ReturnType<Database<Acceptable<Schema>>["findMany"]> {
+    await this.ensureLoaded();
     return this.db.findMany(...args);
   }
 
-  insertOne(
+  async insertOne(
     ...args: Parameters<Database<Acceptable<Schema>>["insertOne"]>
   ): ReturnType<Database<Acceptable<Schema>>["insertOne"]> {
+    await this.ensureLoaded();
     return this.db.insertOne(...args);
   }
 
-  insertMany(
+  async insertMany(
     ...args: Parameters<Database<Acceptable<Schema>>["insertMany"]>
   ): ReturnType<Database<Acceptable<Schema>>["insertMany"]> {
+    await this.ensureLoaded();
     return this.db.insertMany(...args);
   }
 
-  updateOne(
+  async updateOne(
     ...args: Parameters<Database<Acceptable<Schema>>["updateOne"]>
   ): ReturnType<Database<Acceptable<Schema>>["updateOne"]> {
+    await this.ensureLoaded();
     return this.db.updateOne(...args);
   }
 
-  deleteOne(
+  async deleteOne(
     ...args: Parameters<Database<Acceptable<Schema>>["deleteOne"]>
   ): ReturnType<Database<Acceptable<Schema>>["deleteOne"]> {
+    await this.ensureLoaded();
     return this.db.deleteOne(...args);
   }
 
-  deleteMany(
+  async deleteMany(
     ...args: Parameters<Database<Acceptable<Schema>>["deleteMany"]>
   ): ReturnType<Database<Acceptable<Schema>>["deleteMany"]> {
+    await this.ensureLoaded();
     return this.db.deleteMany(...args);
   }
 }
@@ -97,6 +114,7 @@ export class MockAloeDatabase<Schema extends DatabaseDocument> extends BaseAloeD
     },
   ): Promise<MockAloeDatabase<Schema>> {
     const db = new MockAloeDatabase<Schema>(schema);
+    await db.ensureLoaded();
     await db.insertMany(documents);
     return db;
   }
