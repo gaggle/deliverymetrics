236889248b99c1561e69346cb4930cc5b85d0a3c
Jon Lauridsen <mail@jonlauridsen.com>
2023-01-28 22:14:11 +0100

    Fix types.test.ts
    This looks to be the pattern the package recommends

--- types.test.ts
+++ types.test.ts
@@ -1,32 +1,15 @@
-import { assert, Has, IsExact, NotHas } from "dev:conditional-type-checks";
+import { AssertTrue, IsExact } from "dev:conditional-type-checks";
 
 import { Tail, ToTuple, WithOptional, WithRequired } from "./types.ts";
 
-Deno.test("ToTuple", async (t) => {
-  await t.step("extracts an object's keys into a constant array type", () => {
-    type Keys = ToTuple<keyof { foo: string; bar: number }>;
-
-    assert<Has<Keys, ["foo", "bar"]>>(true);
-    assert<NotHas<Keys, ["baz"]>>(true);
-  });
-});
-
-Deno.test("Tail", async (t) => {
-  await t.step("strips the first type", () => {
-    assert<IsExact<Tail<["foo"]>, []>>(true);
-    assert<IsExact<Tail<["foo", "bar"]>, ["bar"]>>(true);
-    assert<IsExact<Tail<["foo", "bar", "baz"]>, ["bar", "baz"]>>(true);
-  });
-});
-
-Deno.test("WithRequired", async (t) => {
-  await t.step("requires a specified property", () => {
-    assert<IsExact<WithRequired<{ foo?: "foo"; bar?: "bar" }, "foo">, { foo: "foo"; bar?: "bar" }>>(true);
-  });
-});
-
-Deno.test("WithOptional", async (t) => {
-  await t.step("makes a specified property optional", () => {
-    assert<IsExact<WithOptional<{ foo: "foo"; bar: "bar" }, "foo">, { foo?: "foo"; bar: "bar" }>>(true);
-  });
-});
+type _ =
+  // ToTuple
+  | AssertTrue<IsExact<ToTuple<keyof { foo: string; bar: number }>, ["foo", "bar"]>>
+  // Tail
+  | AssertTrue<IsExact<Tail<["foo"]>, []>>
+  | AssertTrue<IsExact<Tail<["foo", "bar"]>, ["bar"]>>
+  | AssertTrue<IsExact<Tail<["foo", "bar", "baz"]>, ["bar", "baz"]>>
+  // WithRequired
+  | IsExact<WithRequired<{ foo?: "foo"; bar?: "bar" }, "foo">, { foo: "foo"; bar?: "bar" }>
+  // WithOptional
+  | IsExact<WithOptional<{ foo: "foo"; bar: "bar" }, "foo">, { foo?: "foo"; bar: "bar" }>;
