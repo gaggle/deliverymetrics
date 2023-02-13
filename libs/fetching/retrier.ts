472449534054711ff7f4c1b4c444bb7f946b24bd
Jon Lauridsen <mail@jonlauridsen.com>
2023-02-14 00:06:59 +0100

    Rewrite of Retrier to allow its instances and backoff-functions to emit events

--- libs/fetching/retrier.ts
+++ libs/fetching/retrier.ts
@@ -1,48 +1,75 @@
-type RetryFn = (
-  opts:
-    | { response: Response; error?: never; attemptNumber: number }
-    | { error: Error; response?: never; attemptNumber: number },
-) => boolean | Promise<boolean>
+import { EventEmitter } from "event"
 
-export class Retrier {
+import { sleep } from "../utils/mod.ts"
+
+import { BackoffFn, RetrierEvents } from "./types.ts"
+
+export class Retrier<Events extends RetrierEvents = RetrierEvents> extends EventEmitter<Events> {
   private readonly _fetch: typeof fetch
-  private readonly maxRetries: number
-  private readonly shouldRetry: RetryFn
+  private readonly retries: number
 
-  constructor(opts: {
-    fetch?: typeof fetch
-    maxRetries?: number
-    shouldRetry?: RetryFn
-  } = {}) {
-    this._fetch = opts.fetch || globalThis.fetch
-    this.maxRetries = opts.maxRetries || 3
-    this.shouldRetry = opts.shouldRetry ||
-      (({ response, error }) => !!(!response?.ok || error))
+  constructor(private readonly backoffFn: BackoffFn<Events>, opts: Partial<{
+    retries: number
+    /**
+     * For test-purposes a fetch-like function can be injected. Defaults to global fetch.
+     */
+    _fetch: typeof fetch
+  }> = {}) {
+    super()
+    this._fetch = opts._fetch || globalThis.fetch
+    this.retries = opts.retries !== undefined ? opts.retries : 3
   }
 
   async fetch(...args: Parameters<typeof fetch>): ReturnType<typeof fetch> {
-    const attemptLimitReached = () => {
-      return attemptNumber > this.maxRetries
+    const attemptLimitReached = () => attemptNumber >= this.retries
+
+    const handleRetry = async (opts: { response: Response; error?: never } | { error: Error; response?: never }) => {
+      const backoffDelay = await this.backoffFn({
+        emitter: { emit: this.emit.bind(this) },
+        attemptNumber,
+        ...opts.response ? { response: opts.response } : { error: opts.error },
+      })
+      if (backoffDelay === undefined || attemptLimitReached()) {
+        await this.emit("done", {
+          retry: attemptNumber,
+          retries: this.retries,
+          response: opts.response,
+          error: opts.error,
+        })
+        if (opts.response) {
+          returnResponse = opts.response
+          return
+        } else throw opts.error
+      }
+
+      await this.emit("retrying", { retry: attemptNumber, retries: this.retries, in: backoffDelay })
+      await sleep(backoffDelay)
     }
 
-    let attemptNumber = 0
-    while (!attemptLimitReached()) {
+    let returnResponse: Response | undefined = undefined
+    let attemptNumber = -1
+    while (!returnResponse) {
       try {
-        const response = await this._fetch(...args)
-        const shouldRetry = await this.shouldRetry({ attemptNumber, response })
         attemptNumber++
-        if (!shouldRetry || attemptLimitReached()) {
+        await this.emit("fetching", { retry: attemptNumber, retries: this.retries, request: args })
+        const response = await this._fetch(...args)
+        await this.emit("fetched", { retry: attemptNumber, retries: this.retries, response })
+
+        if (response.ok) {
+          await this.emit("done", {
+            retry: attemptNumber,
+            retries: this.retries,
+            response: response,
+          })
           return response
         }
+
+        await handleRetry({ response })
       } catch (error) {
-        const shouldRetry = await this.shouldRetry({ attemptNumber, error })
-        attemptNumber++
-        if (!shouldRetry || attemptLimitReached()) {
-          throw error
-        }
+        await this.emit("fetched", { retry: attemptNumber, retries: this.retries, error })
+        await handleRetry({ error })
       }
     }
-    throw new Error("Error guard")
-    // ↑ This can't happen but is needed to make Typescript happy
+    return returnResponse
   }
 }
