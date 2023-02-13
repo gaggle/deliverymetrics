4141fcd458486029b7edc0341f1d13ac5e81c4cf
Jon Lauridsen <mail@jonlauridsen.com>
2023-02-14 00:07:34 +0100

    Export Retrier types & retrierFactory in fetching/mod.ts

--- libs/fetching/mod.ts
+++ libs/fetching/mod.ts
@@ -1,2 +1,6 @@
+export type { ExponentialRetrier, RateLimitExponentialRetrier, SimpleRetrier } from "./types.ts"
 export { fetchExhaustively } from "./fetch-exhaustively.ts"
-export { Retrier } from "./retrier.ts"
+export { retrierFactory } from "./retrier-factory.ts"
+
+import { retrierFactory } from "./retrier-factory.ts"
+export type Retrier = ReturnType<typeof retrierFactory>
