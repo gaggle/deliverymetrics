be6462babd1e21d72d91180c20244879f4d71607
Jon Lauridsen <mail@jonlauridsen.com>
2023-05-07 20:01:04 +0200

    Add db-specific schemas to two APIs due to their hard-to-understand structures

--- libs/github/api/stats-code-frequency/github-stats-code-frequency-schema.ts
+++ libs/github/api/stats-code-frequency/github-stats-code-frequency-schema.ts
@@ -3,3 +3,11 @@ import { z } from "zod"
 export const githubStatsCodeFrequencySchema = z.array(z.number().int()).describe("Code Frequency Stat")
 
 export type GithubStatsCodeFrequency = z.infer<typeof githubStatsCodeFrequencySchema>
+
+export const dbCodeFrequencySchema = z.object({
+  time: z.number(),
+  additions: z.number().nonnegative(),
+  deletions: z.number().nonpositive(),
+})
+
+export type DBCodeFrequency = z.infer<typeof dbCodeFrequencySchema>
--- libs/github/api/stats-punch-card/github-stats-punch-card-schema.ts
+++ libs/github/api/stats-punch-card/github-stats-punch-card-schema.ts
@@ -3,3 +3,11 @@ import { z } from "zod"
 export const githubStatsPunchCardSchema = z.array(z.number().int()).describe("Code Frequency Stat")
 
 export type GithubStatsPunchCard = z.infer<typeof githubStatsPunchCardSchema>
+
+export const dbPunchCardSchema = z.object({
+  day: z.number().min(0).max(6),
+  hour: z.number().min(0).max(23),
+  commits: z.number(),
+})
+
+export type DBPunchCard = z.infer<typeof dbPunchCardSchema>
