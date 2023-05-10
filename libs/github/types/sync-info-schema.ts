756a16513982d8ae28523fbcc14332a285aebf11
Jon Lauridsen <mail@jonlauridsen.com>
2023-05-10 22:56:53 +0200

    Extract `syncInfoTypes` to have a runtime list

--- libs/github/types/sync-info-schema.ts
+++ libs/github/types/sync-info-schema.ts
@@ -1,20 +1,25 @@
 import * as z from "zod"
 
+const syncInfoZodTypes = [
+  z.literal("action-run"),
+  z.literal("action-workflow"),
+  z.literal("commit"),
+  z.literal("pull"),
+  z.literal("pull-commit"),
+  z.literal("release"),
+  z.literal("stats-code-frequency"),
+  z.literal("stats-commit-activity"),
+  z.literal("stats-contributors"),
+  z.literal("stats-participation"),
+  z.literal("stats-punch-card"),
+] as const
+
+export const syncInfoTypes = syncInfoZodTypes.map((el) => el.value)
+
 export const syncInfoSchema = z.object({
-  type: z.union([
-    z.literal("action-run"),
-    z.literal("action-workflow"),
-    z.literal("commit"),
-    z.literal("pull"),
-    z.literal("pull-commit"),
-    z.literal("release"),
-    z.literal("stats-code-frequency"),
-    z.literal("stats-commit-activity"),
-    z.literal("stats-contributors"),
-    z.literal("stats-participation"),
-    z.literal("stats-punch-card"),
-  ]),
+  type: z.union(syncInfoZodTypes),
   createdAt: z.number(),
   updatedAt: z.number().optional(),
 })
+
 export type SyncInfo = z.infer<typeof syncInfoSchema>
