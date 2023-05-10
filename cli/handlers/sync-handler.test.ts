59aed003fba85699d64dad0fa45daf37540c1dd6
Jon Lauridsen <mail@jonlauridsen.com>
2023-05-10 22:59:05 +0200

    Generate a report of what didn't sync if sync-handler hits errors

--- cli/handlers/sync-handler.test.ts
+++ cli/handlers/sync-handler.test.ts
@@ -1,7 +1,10 @@
-import { assertSpyCallArgs, assertSpyCalls, stub } from "dev:mock"
+import { assertEquals } from "dev:asserts"
+import { StringWriter } from "std:io"
+import { assertSpyCallArgs, assertSpyCalls, Stub, stub } from "dev:mock"
 
 import { getFakeGithubPull } from "../../libs/github/api/pulls/mod.ts"
 
+import { GithubClient } from "../../libs/github/types/mod.ts"
 import { createFakeGithubClient } from "../../libs/github/testing/mod.ts"
 
 import { withStubs } from "../../libs/dev-utils.ts"
@@ -47,8 +50,43 @@ Deno.test("syncHandler", async (t) => {
   })
 })
 
-Deno.test("fullGithubSync", async (t) => {
-  await t.step("calls #sync methods", async () => {
+type FullGithubSyncStubs = {
+  syncActionRuns: Stub
+  syncActionWorkflows: Stub
+  syncCommits: Stub
+  syncPullCommits: Stub
+  syncPulls: Stub
+  syncReleases: Stub
+  syncStatsCodeFrequency: Stub
+  syncStatsCommitActivity: Stub
+  syncStatsContributors: Stub
+  syncStatsParticipation: Stub
+  syncStatsPunchCard: Stub
+}
+
+async function withFullGithubSync(
+  t: Deno.TestContext,
+  name: string,
+  callable: (
+    opts:
+      & { t: Deno.TestContext; client: GithubClient; output: string; result: PromiseSettledResult<void> }
+      & FullGithubSyncStubs,
+  ) => void | Promise<void>,
+  opts: Partial<{
+    rejectSyncActionRuns: boolean
+    rejectSyncActionWorkflows: boolean
+    rejectSyncCommits: boolean
+    rejectSyncPullCommits: boolean
+    rejectSyncPulls: boolean
+    rejectSyncReleases: boolean
+    rejectSyncStatsCodeFrequency: boolean
+    rejectSyncStatsCommitActivity: boolean
+    rejectSyncStatsContributors: boolean
+    rejectSyncStatsParticipation: boolean
+    rejectSyncStatsPunchCard: boolean
+  }> = {},
+): Promise<void> {
+  await t.step(name, async (t) => {
     const client = await createFakeGithubClient()
     await withStubs(
       async (
@@ -57,35 +95,190 @@ Deno.test("fullGithubSync", async (t) => {
         syncCommits,
         syncPullCommits,
         syncPulls,
+        syncReleases,
         syncStatsCodeFrequency,
         syncStatsCommitActivity,
         syncStatsContributors,
         syncStatsParticipation,
         syncStatsPunchCard,
       ) => {
-        await fullGithubSync(client)
-        assertSpyCalls(syncActionRuns, 1)
-        assertSpyCalls(syncActionWorkflows, 1)
-        assertSpyCalls(syncCommits, 1)
-        assertSpyCalls(syncPullCommits, 1)
-        assertSpyCalls(syncPulls, 1)
-        assertSpyCalls(syncStatsCodeFrequency, 1)
-        assertSpyCalls(syncStatsCommitActivity, 1)
-        assertSpyCalls(syncStatsContributors, 1)
-        assertSpyCalls(syncStatsParticipation, 1)
-        assertSpyCalls(syncStatsPunchCard, 1)
+        const writer = new StringWriter()
+        const [result] = await Promise.allSettled([fullGithubSync(client, { _stdOutLike: writer })])
+
+        await callable({
+          t,
+          client,
+          output: writer.toString(),
+          result,
+          syncActionRuns,
+          syncActionWorkflows,
+          syncCommits,
+          syncPullCommits,
+          syncPulls,
+          syncReleases,
+          syncStatsCodeFrequency,
+          syncStatsCommitActivity,
+          syncStatsContributors,
+          syncStatsParticipation,
+          syncStatsPunchCard,
+        })
       },
-      stub(client, "syncActionRuns", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncActionWorkflows", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncCommits", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncPullCommits", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncPulls", () => Promise.resolve({ syncedAt: 0, syncedPulls: [getFakeGithubPull()] })),
-      stub(client, "syncReleases", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncStatsCodeFrequency", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncStatsCommitActivity", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncStatsContributors", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncStatsParticipation", () => Promise.resolve({ syncedAt: 0 })),
-      stub(client, "syncStatsPunchCard", () => Promise.resolve({ syncedAt: 0 })),
+      stub(client, "syncActionRuns", () => {
+        client.emit("progress", { type: "action-run" })
+        if (opts.rejectSyncActionRuns) {
+          return Promise.reject(new Error("rejectSyncActionRuns"))
+        } else {
+          client.emit("finished", { type: "action-run" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncActionWorkflows", () => {
+        client.emit("progress", { type: "action-workflow" })
+        if (opts.rejectSyncActionWorkflows) {
+          return Promise.reject(new Error("rejectSyncActionWorkflows"))
+        } else {
+          client.emit("finished", { type: "action-workflow" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncCommits", () => {
+        client.emit("progress", { type: "commit" })
+        if (opts.rejectSyncCommits) {
+          return Promise.reject(new Error("rejectSyncCommits"))
+        } else {
+          client.emit("finished", { type: "commit" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncPullCommits", () => {
+        client.emit("progress", { type: "pull-commit" })
+        if (opts.rejectSyncPullCommits) {
+          return Promise.reject(new Error("rejectSyncPullCommits"))
+        } else {
+          client.emit("finished", { type: "pull-commit" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncPulls", () => {
+        client.emit("progress", { type: "pull" })
+        if (opts.rejectSyncPulls) {
+          return Promise.reject(new Error("rejectSyncPulls"))
+        } else {
+          client.emit("finished", { type: "pull" })
+          return Promise.resolve({ syncedAt: 0, syncedPulls: [getFakeGithubPull()] })
+        }
+      }),
+      stub(client, "syncReleases", () => {
+        client.emit("progress", { type: "release" })
+        if (opts.rejectSyncReleases) {
+          return Promise.reject(new Error("rejectSyncReleases"))
+        } else {
+          client.emit("finished", { type: "release" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncStatsCodeFrequency", () => {
+        client.emit("progress", { type: "stats-code-frequency" })
+        if (opts.rejectSyncStatsCodeFrequency) {
+          return Promise.reject(new Error("rejectSyncStatsCodeFrequency"))
+        } else {
+          client.emit("finished", { type: "stats-code-frequency" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncStatsCommitActivity", () => {
+        client.emit("progress", { type: "stats-commit-activity" })
+        if (opts.rejectSyncStatsCommitActivity) {
+          return Promise.reject(new Error("rejectSyncStatsCommitActivity"))
+        } else {
+          client.emit("finished", { type: "stats-commit-activity" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncStatsContributors", () => {
+        client.emit("progress", { type: "stats-contributors" })
+        if (opts.rejectSyncStatsContributors) {
+          return Promise.reject(new Error("rejectSyncStatsContributors"))
+        } else {
+          client.emit("finished", { type: "stats-contributors" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncStatsParticipation", () => {
+        client.emit("progress", { type: "stats-participation" })
+        if (opts.rejectSyncStatsParticipation) {
+          return Promise.reject(new Error("rejectSyncStatsParticipation"))
+        } else {
+          client.emit("finished", { type: "stats-participation" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
+      stub(client, "syncStatsPunchCard", () => {
+        client.emit("progress", { type: "stats-punch-card" })
+        if (opts.rejectSyncStatsPunchCard) {
+          return Promise.reject(new Error("rejectSyncStatsPunchCard"))
+        } else {
+          client.emit("finished", { type: "stats-punch-card" })
+          return Promise.resolve({ syncedAt: 0 })
+        }
+      }),
     )
   })
+}
+
+Deno.test("fullGithubSync", async (t) => {
+  await withFullGithubSync(t, "resolves just fine", (params) => {
+    assertEquals(params.result.status, "fulfilled") // It resolved 👍
+  })
+
+  await withFullGithubSync(t, "calls #sync* methods", (params) => {
+    assertSpyCalls(params.syncActionRuns, 1)
+    assertSpyCalls(params.syncActionWorkflows, 1)
+    assertSpyCalls(params.syncCommits, 1)
+    assertSpyCalls(params.syncPullCommits, 1)
+    assertSpyCalls(params.syncPulls, 1)
+    assertSpyCalls(params.syncReleases, 1)
+    assertSpyCalls(params.syncStatsCodeFrequency, 1)
+    assertSpyCalls(params.syncStatsCommitActivity, 1)
+    assertSpyCalls(params.syncStatsContributors, 1)
+    assertSpyCalls(params.syncStatsParticipation, 1)
+    assertSpyCalls(params.syncStatsPunchCard, 1)
+  })
+
+  await withFullGithubSync(t, "outputs a status report", ({ output }) => {
+    assertEquals(
+      output,
+      `Legend: r=action-run, w=action-workflow, c=commit, p=pull|pull-commit, R=release, s=stats
+rwcpRp
+✅ 
+`,
+    )
+  })
+
+  await withFullGithubSync(t, "rejects when any of the syncs fail", ({ result }) => {
+    assertEquals(result.status, "rejected")
+  }, { rejectSyncActionRuns: true })
+
+  await withFullGithubSync(t, "outputs a report with the sync that fails", ({ output }) => {
+    assertEquals(
+      output,
+      `Legend: r=action-run, w=action-workflow, c=commit, p=pull|pull-commit, R=release, s=stats
+rwcpRp
+❌  action-run failed to sync, reason: Error: rejectSyncActionRuns
+`,
+    )
+  }, { rejectSyncActionRuns: true })
+
+  await withFullGithubSync(t, "does not sync pull-commits if pulls fails", ({ result, output, syncPullCommits }) => {
+    assertEquals(result.status, "rejected")
+    assertSpyCalls(syncPullCommits, 0)
+    assertEquals(
+      output,
+      `Legend: r=action-run, w=action-workflow, c=commit, p=pull|pull-commit, R=release, s=stats
+rwcpR
+❌  pull failed to sync, reason: Error: rejectSyncPulls
+❌  pull-commit failed to sync, reason: pending
+`,
+    )
+  }, { rejectSyncPulls: true })
 })
--- cli/handlers/sync-handler.ts
+++ cli/handlers/sync-handler.ts
@@ -1,10 +1,10 @@
 import { join } from "std:path"
 
-import { getGithubClient, GithubClient } from "../../libs/github/mod.ts"
+import { getGithubClient, GithubClient, SyncInfo, syncInfoTypes } from "../../libs/github/mod.ts"
 
 import { AbortError } from "../../libs/errors.ts"
 
-import { dot } from "./formatting.ts"
+import { dot, write } from "./formatting.ts"
 
 export type SyncSpec = {
   type: "github"
@@ -24,7 +24,7 @@ export type SyncSpec = {
 export async function syncHandler(
   opts: { syncSpecs: Array<SyncSpec>; cacheRoot: string; signal?: AbortSignal },
 ): Promise<void> {
-  console.log("Syncing...")
+  write("Syncing...")
   for (const syncSpec of opts.syncSpecs) {
     switch (syncSpec.type) {
       case "github":
@@ -43,7 +43,7 @@ export async function syncHandler(
         )
         break
       case "jira":
-        console.log("JIRA", syncSpec.project)
+        write(`JIRA ${syncSpec.project}`)
         break
     }
   }
@@ -56,7 +56,9 @@ const dayInMs = 24 * 60 * 60 * 1000
 
 export async function fullGithubSync(
   github: GithubClient,
-  { maxDaysToSync, signal }: { maxDaysToSync?: number; signal?: AbortSignal } = {},
+  { maxDaysToSync, signal, _stdOutLike }: Partial<
+    { maxDaysToSync: number; signal: AbortSignal; _stdOutLike: Deno.WriterSync }
+  > = {},
 ) {
   let syncNewerThan: number | undefined
   if (maxDaysToSync !== undefined) {
@@ -66,53 +68,81 @@ export async function fullGithubSync(
   github.on("progress", ({ type }) => {
     switch (type) {
       case "action-run":
-        return dot("r")
+        return dot({ char: "r", _stdOutLike })
       case "action-workflow":
-        return dot("w")
+        return dot({ char: "w", _stdOutLike })
       case "commit":
-        return dot("c")
+        return dot({ char: "c", _stdOutLike })
       case "pull":
-        return dot("p")
+        return dot({ char: "p", _stdOutLike })
       case "pull-commit":
-        return dot("p")
+        return dot({ char: "p", _stdOutLike })
       case "release":
-        return dot("R")
+        return dot({ char: "R", _stdOutLike })
     }
   })
 
   github.on("finished", ({ type }) => {
     if (type.startsWith("stats-")) {
-      return dot("s")
+      return dot({ char: "s" })
     }
   })
 
-  console.log(
+  const errorRegistry = syncInfoTypes.reduce((acc, curr) => ({
+    ...acc,
+    [curr]: "pending",
+  }), {} as Record<SyncInfo["type"], Error | "pending">)
+
+  async function errorRegistrar<T>(name: SyncInfo["type"], cb: () => Promise<T>): Promise<T> {
+    try {
+      const result = await cb()
+      delete errorRegistry[name]
+      return result
+    } catch (err) {
+      errorRegistry[name] = err
+      throw new RegisteredError()
+    }
+  }
+
+  write(
     "Legend: r=action-run, w=action-workflow, c=commit, p=pull|pull-commit, R=release, s=stats",
+    { _stdOutLike },
   )
   try {
-    const result = await Promise.allSettled([
-      github.syncActionRuns({ newerThan: syncNewerThan, signal }),
-      github.syncActionWorkflows({ signal }),
-      github.syncCommits({ newerThan: syncNewerThan, signal }),
-      github.syncPulls({ newerThan: syncNewerThan, signal })
-        .then((result) => github.syncPullCommits(result.syncedPulls, { signal })),
-      github.syncReleases({ newerThan: syncNewerThan, signal }),
-      github.syncStatsCodeFrequency({ signal }),
-      github.syncStatsCommitActivity({ signal }),
-      github.syncStatsContributors({ signal }),
-      github.syncStatsParticipation({ signal }),
-      github.syncStatsPunchCard({ signal }),
+    await Promise.allSettled([
+      errorRegistrar("action-run", () => github.syncActionRuns({ newerThan: syncNewerThan, signal })),
+      errorRegistrar("action-workflow", () => github.syncActionWorkflows({ signal })),
+      errorRegistrar("commit", () => github.syncCommits({ newerThan: syncNewerThan, signal })),
+      errorRegistrar("pull", () => github.syncPulls({ newerThan: syncNewerThan, signal }))
+        .then((result) => errorRegistrar("pull-commit", () => github.syncPullCommits(result.syncedPulls, { signal }))),
+      errorRegistrar("release", () => github.syncReleases({ newerThan: syncNewerThan, signal })),
+      errorRegistrar("stats-code-frequency", () => github.syncStatsCodeFrequency({ signal })),
+      errorRegistrar("stats-commit-activity", () => github.syncStatsCommitActivity({ signal })),
+      errorRegistrar("stats-contributors", () => github.syncStatsContributors({ signal })),
+      errorRegistrar("stats-participation", () => github.syncStatsParticipation({ signal })),
+      errorRegistrar("stats-punch-card", () => github.syncStatsPunchCard({ signal })),
     ])
-    const report = result.map((el) => el.status === "fulfilled" ? "✅" : `❌${el.reason}`)
-    if (result.filter((el) => el.status === "rejected").length > 0) {
-      throw new Error(`Sync failures occurred:\n${report.join("\n")}`)
-    }
+    write("", { _stdOutLike }) // New-line to move on from `dot` calls
   } catch (err) {
     if (err instanceof AbortError) {
-      // Nothing more to do
+      return // Nothing more to do
     } else {
       throw err
     }
+  }
+
+  if (Object.keys(errorRegistry).length > 0) {
+    const msg = Object.entries(errorRegistry).map(([name, reason]) => `❌  ${name} failed to sync, reason: ${reason}`)
+    write(msg.join("\n"), { _stdOutLike })
+    throw new RegisteredError(msg.join("\n"))
+  } else {
+    write("✅ ", { _stdOutLike })
+  }
+}
+
+export class RegisteredError extends Error {
+  constructor(...args: ConstructorParameters<typeof Error>) {
+    super(...args)
   }
 }
 
