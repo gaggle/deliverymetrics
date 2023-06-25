6277f90864713b4315b7c267bc19e71832abf15a
Jon Lauridsen <mail@jonlauridsen.com>
2023-06-25 18:27:33 +0200

    Support getting actionRuns from a specific branch

--- libs/github/api/github-rest-api-spec.ts
+++ libs/github/api/github-rest-api-spec.ts
@@ -18,10 +18,13 @@ export const githubRestSpec = {
    * https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#list-workflow-runs-for-a-repository
    */
   actionRuns: {
-    getUrl: (owner: string, repo: string) => {
+    getUrl: (owner: string, repo: string, branch?: string) => {
       const url = new URL(`https://api.github.com/repos/${owner}/${repo}/actions/runs`)
       url.searchParams.set("per_page", "50")
       // ↑ Occasionally the API would return 502 Bad Gateway when per_page=100 (despite retrying)
+      if (branch) {
+        url.searchParams.set("branch", branch)
+      }
       return url.toString()
     },
     schema: z.object({ total_count: z.number().int(), workflow_runs: z.array(githubActionRunSchema) }),
