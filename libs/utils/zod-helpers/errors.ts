733c4cf7050c32539936f8b449a7745d3d60a191
Jon Lauridsen <mail@jonlauridsen.com>
2023-06-26 00:36:03 +0200

    Add support for request & response in EnrichedZodError

--- libs/utils/zod-helpers/errors.ts
+++ libs/utils/zod-helpers/errors.ts
@@ -1,22 +1,41 @@
-import { getLogger } from "std:log"
 import { z } from "zod"
+
+import { isDebugLoggingActive, toCurlCommand } from "../mod.ts"
 
 import { stringifyZodError } from "./stringify-zod-error.ts"
 
 export class EnrichedZodError extends z.ZodError {
-  readonly #data?: unknown
+  readonly #data?: string
+  readonly #request?: Request
+  readonly #response?: Response
 
-  constructor(issues: z.ZodIssue[], opts: { data?: unknown } = {}) {
+  constructor(issues: z.ZodIssue[], opts: {
+    data?: string | unknown
+    request?: Request
+    response?: Response
+  } = {}) {
     super(issues)
     this.name = "EnrichedZodError"
-    this.#data = opts.data
+    this.#data = typeof opts.data === "string" ? opts.data : JSON.stringify(opts.data, null, 2)
+    this.#request = opts.request
+    this.#response = opts.response
   }
 
   get message() {
     let msg = stringifyZodError(this.issues)
-    if (getLogger().level <= 10) {
-      msg += `, data: ${JSON.stringify(this.#data, null, 2)}`
+
+    if (this.#request) {
+      msg += `\n  ↳ request: ${toCurlCommand(this.#request)}`
     }
+
+    if (this.#response) {
+      msg += `\n  ↳ response: ${this.#response.status} ${this.#response.statusText}`
+    }
+
+    if (isDebugLoggingActive()) {
+      msg += `\n  ↳ data: ${this.#data}`
+    }
+
     return msg
   }
 }
