c93df2629cac501738c30bd7c10897b2b446d608
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-03 08:38:14 +0200

    Delete libs/fetching (libs/fetching2 is now the only implementation)

--- libs/fetching/mod.ts
+++
@@ -1,6 +0,0 @@
-export type { ExponentialRetrier, RateLimitExponentialRetrier, SimpleRetrier } from "./types.ts"
-export { fetchExhaustively } from "./fetch-exhaustively.ts"
-export { retrierFactory } from "./retrier-factory.ts"
-
-import { retrierFactory } from "./retrier-factory.ts"
-export type Retrier = ReturnType<typeof retrierFactory>
