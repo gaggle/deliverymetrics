4c112a494d4a116cad0f7206d9d3d05d6caaf984
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-04 22:22:25 +0200

    Add cli/csv githubCommitsAsCsv function

---
+++ cli/csv/csv-github-commit-headers.ts
@@ -0,0 +1,26 @@
+import { githubCommitSchema } from "../../libs/github/api/commits/mod.ts"
+
+import { yieldCommitData } from "../../libs/metrics/mod.ts"
+import { extractZodSchemaKeys, flattenObject, stringifyObject } from "../../libs/utils/mod.ts"
+
+const extraHeaders = [
+  "Commit Co-Authors",
+] as const
+
+export const githubCommitHeaders = [
+  ...extraHeaders,
+  ...Object.keys(flattenObject(extractZodSchemaKeys(githubCommitSchema))).sort(),
+]
+
+export type GithubCommitRow = Record<typeof githubCommitHeaders[number], string>
+
+export async function* githubCommitsAsCsv(
+  iter: ReturnType<typeof yieldCommitData>,
+): AsyncGenerator<GithubCommitRow> {
+  for await (const el of iter) {
+    yield {
+      "Commit Co-Authors": el.coauthors.join("; "),
+      ...stringifyObject(flattenObject(el.commit), { stringifyUndefined: true }),
+    }
+  }
+}
--- cli/csv/mod.ts
+++ cli/csv/mod.ts
@@ -1,5 +1,6 @@
 export * from "./csv-github-action-run-headers.ts"
 export * from "./csv-github-action-workflows-headers.ts"
+export * from "./csv-github-commit-headers.ts"
 export * from "./csv-github-pull-commit-headers.ts"
 export * from "./csv-github-pull-headers.ts"
 export * from "./csv-pull-request-histogram-headers.ts"
