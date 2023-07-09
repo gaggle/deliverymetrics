2b987bedb7730870cba717b18de8c31cc2c99131
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-09 11:11:12 +0200

    Add libs/metrics/github-release-data

---
+++ libs/metrics/github-release-data.ts
@@ -0,0 +1,41 @@
+import { GithubRelease } from "../github/api/releases/mod.ts"
+
+import { ReadonlyGithubClient } from "../github/mod.ts"
+
+import { AbortError } from "../errors.ts"
+import { daysBetween } from "../utils/date-utils.ts"
+
+type YieldReleaseData = {
+  release: GithubRelease
+}
+
+export async function* yieldReleaseData(
+  gh: ReadonlyGithubClient,
+  { publishedMaxDaysAgo, signal }: Partial<
+    {
+      publishedMaxDaysAgo: number
+      signal: AbortSignal
+    }
+  > = {},
+): AsyncGenerator<YieldReleaseData> {
+  const latestSync = await gh.findLatestSync({ type: "release" })
+  if (!latestSync) return
+
+  for await (const release of gh.findReleases()) {
+    if (signal?.aborted) {
+      throw new AbortError()
+    }
+
+    if (
+      publishedMaxDaysAgo !== undefined &&
+      release.published_at &&
+      daysBetween(new Date(release.published_at), new Date(latestSync.updatedAt!)) > (publishedMaxDaysAgo || Infinity)
+    ) {
+      continue
+    }
+
+    yield {
+      release,
+    }
+  }
+}
