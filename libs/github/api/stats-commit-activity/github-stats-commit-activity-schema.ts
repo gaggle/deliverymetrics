80d959d53a5c049f75aaccf24b5e924c383f2f5c
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-10 01:10:10 +0200

    Narrow githubStatsCommitActivitySchema.days to be a tuple

--- libs/github/api/stats-commit-activity/github-stats-commit-activity-schema.ts
+++ libs/github/api/stats-commit-activity/github-stats-commit-activity-schema.ts
@@ -2,7 +2,15 @@ import { z } from "zod"
 
 export const githubStatsCommitActivitySchema = z
   .object({
-    days: z.array(z.number().int()),
+    days: z.tuple([
+      z.number().int(), // sunday
+      z.number().int(), // monday
+      z.number().int(), // tuesday
+      z.number().int(), // wednesday
+      z.number().int(), // thursday
+      z.number().int(), // friday
+      z.number().int(), // saturday
+    ]),
     total: z.number().int(),
     week: z.number().int(),
   })
