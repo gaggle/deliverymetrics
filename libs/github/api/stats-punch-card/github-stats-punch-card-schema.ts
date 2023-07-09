a1b62d174f4a3f33b1088faccff71ba2f18c154c
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-09 13:10:20 +0200

    Specify the possible values in DBPunchCard type for day and hour

--- libs/github/api/stats-punch-card/github-stats-punch-card-schema.ts
+++ libs/github/api/stats-punch-card/github-stats-punch-card-schema.ts
@@ -11,3 +11,8 @@ export const dbPunchCardSchema = z.object({
 })
 
 export type DBPunchCard = z.infer<typeof dbPunchCardSchema>
+
+export type DBPunchCardRead = DBPunchCard & {
+  day: 0 | 1 | 2 | 3 | 4 | 5 | 6
+  hour: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23
+}
