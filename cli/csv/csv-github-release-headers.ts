9ed2603cd3cc9458992b0384279823418cf12373
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-09 11:11:30 +0200

    Add cli/csv/csv-github-release-headers.ts

---
+++ cli/csv/csv-github-release-headers.ts
@@ -0,0 +1,23 @@
+import { githubReleaseSchema } from "../../libs/github/api/releases/mod.ts"
+
+import { yieldReleaseData } from "../../libs/metrics/mod.ts"
+import { extractZodSchemaKeys, flattenObject, stringifyObject } from "../../libs/utils/mod.ts"
+
+const extraHeaders = [] as const
+
+export const githubReleaseHeaders = [
+  ...extraHeaders,
+  ...Object.keys(flattenObject(extractZodSchemaKeys(githubReleaseSchema))).sort(),
+]
+
+export type GithubReleaseRow = Record<typeof githubReleaseHeaders[number], string>
+
+export async function* githubReleasesAsCsv(
+  iter: ReturnType<typeof yieldReleaseData>,
+): AsyncGenerator<GithubReleaseRow> {
+  for await (const el of iter) {
+    yield {
+      ...stringifyObject(flattenObject(el.release), { stringifyUndefined: true }),
+    }
+  }
+}
