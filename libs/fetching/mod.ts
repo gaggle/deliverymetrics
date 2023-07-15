a3e313292633666d3045703ff12e254350e3e905
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-16 00:49:01 +0200

    Export useful fetchExhaustively types

--- libs/fetching/mod.ts
+++ libs/fetching/mod.ts
@@ -1,3 +1,5 @@
 export { fetchExhaustively } from "./fetch-exhaustively.ts"
+export type { FetchExhaustivelyOpts, FetchExhaustivelyProgress } from "./fetch-exhaustively.ts"
+
 export { fetchWithRetry } from "./fetch-with-retry.ts"
 export { githubBackoff, rateLimitAwareBackoff } from "./rate-limit-aware-backoff.ts"
