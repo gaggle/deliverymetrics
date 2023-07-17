2de1f9ced9df207eea5734b9458132cc08501527
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-17 21:17:13 +0200

    Group GitHub reporting into one function

--- cli/handlers/report-handler.ts
+++ cli/handlers/report-handler.ts
@@ -112,177 +112,184 @@ export async function reportHandler(
 
   const jobs: Array<Promise<unknown>> = []
 
-  jobs.push(limit(async () => {
-    await timeCtx("action-run, action-workflows", async () => {
-      const actionRunGens: Array<AsyncGenerator<{ actionRun: GithubActionRun; workflow: GithubActionWorkflow }>> = []
-      await writeCSVToFile(
-        join(outputDir, `github-action-workflows-data.csv`),
-        githubActionWorkflowAsCsv(
-          inspectIter((el) => {
-            actionRunGens.push(mapIter((actionRun) => ({
-              actionRun: actionRun,
-              workflow: el.actionWorkflow,
-            }), el.actionRunGenerator))
-            dot()
-          }, yieldActionData(gh, { actionRun: { maxDays: dataTimeframe, branch: github.actionRuns?.branch }, signal })),
-        ),
-        {
-          header: reorganizeHeaders(githubActionWorkflowHeaders, {
-            ignoreHeaders: github.actionWorkflows?.ignoreHeaders,
-            headerOrder: github.actionWorkflows?.headerOrder,
-          }),
-        },
-      )
-
-      await writeCSVToFile(
-        join(outputDir, `github-action-runs-data.csv`),
-        githubActionRunAsCsv(inspectIter(() => dot(), mergeAsyncGenerators(...actionRunGens))),
-        {
-          header: reorganizeHeaders(githubActionRunHeaders, {
-            ignoreHeaders: github.actionRuns?.ignoreHeaders,
-            headerOrder: github.actionRuns?.headerOrder,
-          }),
-        },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("commits", async () => {
-      await writeCSVToFile(
-        join(outputDir, "github-commits-data.csv"),
-        githubCommitsAsCsv(
-          yieldCommitData(gh, { authoredMaxDaysAgo: dataTimeframe, committedMaxDaysAgo: dataTimeframe, signal }),
-        ),
-        { header: githubCommitHeaders },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("pull-commits, pulls", async () => {
-      const pullCommits: Array<BoundGithubPullCommit> = []
-      await writeCSVToFile(
-        join(outputDir, `github-pulls-data.csv`),
-        githubPullsAsCsv(
-          inspectIter(
-            (el) => {
-              pullCommits.push(...el.commits)
-              dot()
-            },
-            yieldPullRequestData(gh, {
-              maxDays: dataTimeframe,
-              excludeLabels: github.pulls?.ignoreLabels,
-              includeCancelled: github.pulls?.includeCancelled,
-              signal,
-            }),
-          ),
-        ),
-        {
-          header: reorganizeHeaders(githubPullHeaders, {
-            ignoreHeaders: github.pulls?.ignoreHeaders,
-            headerOrder: github.pulls?.headerOrder,
-          }),
-        },
-      )
-
-      await writeCSVToFile(
-        join(outputDir, `github-pull-commits-data.csv`),
-        githubPullCommitsAsCsv(inspectIter(
-          () => dot(),
-          arrayToAsyncGenerator(sortPullCommitsByKey(pullCommits, "commit.author")),
-        )),
-        {
-          header: reorganizeHeaders(githubPullCommitHeaders, {
-            ignoreHeaders: github.pullCommits?.ignoreHeaders,
-            headerOrder: github.pullCommits?.headerOrder,
-          }),
-        },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("releases", async () => {
-      await writeCSVToFile(
-        join(outputDir, "github-releases-data.csv"),
-        githubReleasesAsCsv(yieldReleaseData(gh, { publishedMaxDaysAgo: dataTimeframe, signal })),
-        { header: githubReleaseHeaders },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("stats-code-frequency", async () => {
-      await writeCSVToFile(
-        join(outputDir, "github-stats-code-frequency-data.csv"),
-        githubStatsCodeFrequenciesAsCsv(yieldStatsCodeFrequency(gh, { maxDays: dataTimeframe, signal })),
-        { header: githubStatsCodeFrequencyHeaders },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("stats-commit-activity", async () => {
-      await writeCSVToFile(
-        join(outputDir, "github-stats-commit-activity-data.csv"),
-        githubStatsCommitActivityAsCsv(yieldStatsCommitActivity(gh, { maxDays: dataTimeframe, signal })),
-        { header: githubStatsCommitActivityHeaders },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("stats-contributors", async () => {
-      await writeCSVToFile(
-        join(outputDir, "github-stats-contributors-data.csv"),
-        githubStatsContributorsAsCsv(yieldStatsContributors(gh, { maxDays: dataTimeframe, signal })),
-        { header: githubStatsContributorsHeaders },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("stats-participation", async () => {
-      await writeCSVToFile(
-        join(outputDir, "github-stats-participation-data.csv"),
-        githubStatsParticipationAsCsv(yieldStatsParticipation(gh, { signal })),
-        { header: githubStatsParticipationHeaders },
-      )
-    })
-  }))
-
-  jobs.push(limit(async () => {
-    await timeCtx("stats-punch-card", async () => {
-      await writeCSVToFile(
-        join(outputDir, "github-stats-punch-card-data.csv"),
-        githubStatsPunchCardAsCsv(yieldStatsPunchCard(gh, { signal })),
-        { header: githubStatsPunchCardHeaders },
-      )
-    })
-  }))
-
-  for (const { mode, maxDays } of yieldHistogramTimeframes()) {
+  function queueGitHubReportJobs() {
     jobs.push(limit(async () => {
-      await timeCtx(`${mode} histograms`, async () => {
+      await timeCtx("action-run, action-workflows", async () => {
+        const actionRunGens: Array<AsyncGenerator<{ actionRun: GithubActionRun; workflow: GithubActionWorkflow }>> = []
         await writeCSVToFile(
-          join(outputDir, `pull-request-lead-times-${mode}.csv`),
-          pullRequestHistogramAsCsv(
+          join(outputDir, `github-action-workflows-data.csv`),
+          githubActionWorkflowAsCsv(
             inspectIter(
-              () => dot(),
-              yieldPullRequestHistogram(gh, {
-                mode,
-                maxDays,
+              (el) => {
+                actionRunGens.push(mapIter((actionRun) => ({
+                  actionRun: actionRun,
+                  workflow: el.actionWorkflow,
+                }), el.actionRunGenerator))
+                dot()
+              },
+              yieldActionData(gh, { actionRun: { maxDays: dataTimeframe, branch: github.actionRuns?.branch }, signal }),
+            ),
+          ),
+          {
+            header: reorganizeHeaders(githubActionWorkflowHeaders, {
+              ignoreHeaders: github.actionWorkflows?.ignoreHeaders,
+              headerOrder: github.actionWorkflows?.headerOrder,
+            }),
+          },
+        )
+
+        await writeCSVToFile(
+          join(outputDir, `github-action-runs-data.csv`),
+          githubActionRunAsCsv(inspectIter(() => dot(), mergeAsyncGenerators(...actionRunGens))),
+          {
+            header: reorganizeHeaders(githubActionRunHeaders, {
+              ignoreHeaders: github.actionRuns?.ignoreHeaders,
+              headerOrder: github.actionRuns?.headerOrder,
+            }),
+          },
+        )
+      })
+    }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("commits", async () => {
+        await writeCSVToFile(
+          join(outputDir, "github-commits-data.csv"),
+          githubCommitsAsCsv(
+            yieldCommitData(gh, { authoredMaxDaysAgo: dataTimeframe, committedMaxDaysAgo: dataTimeframe, signal }),
+          ),
+          { header: githubCommitHeaders },
+        )
+      })
+    }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("pull-commits, pulls", async () => {
+        const pullCommits: Array<BoundGithubPullCommit> = []
+        await writeCSVToFile(
+          join(outputDir, `github-pulls-data.csv`),
+          githubPullsAsCsv(
+            inspectIter(
+              (el) => {
+                pullCommits.push(...el.commits)
+                dot()
+              },
+              yieldPullRequestData(gh, {
+                maxDays: dataTimeframe,
                 excludeLabels: github.pulls?.ignoreLabels,
+                includeCancelled: github.pulls?.includeCancelled,
                 signal,
               }),
             ),
           ),
-          { header: pullRequestHistogramHeaders.slice() },
+          {
+            header: reorganizeHeaders(githubPullHeaders, {
+              ignoreHeaders: github.pulls?.ignoreHeaders,
+              headerOrder: github.pulls?.headerOrder,
+            }),
+          },
+        )
+
+        await writeCSVToFile(
+          join(outputDir, `github-pull-commits-data.csv`),
+          githubPullCommitsAsCsv(inspectIter(
+            () => dot(),
+            arrayToAsyncGenerator(sortPullCommitsByKey(pullCommits, "commit.author")),
+          )),
+          {
+            header: reorganizeHeaders(githubPullCommitHeaders, {
+              ignoreHeaders: github.pullCommits?.ignoreHeaders,
+              headerOrder: github.pullCommits?.headerOrder,
+            }),
+          },
         )
       })
     }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("releases", async () => {
+        await writeCSVToFile(
+          join(outputDir, "github-releases-data.csv"),
+          githubReleasesAsCsv(yieldReleaseData(gh, { publishedMaxDaysAgo: dataTimeframe, signal })),
+          { header: githubReleaseHeaders },
+        )
+      })
+    }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("stats-code-frequency", async () => {
+        await writeCSVToFile(
+          join(outputDir, "github-stats-code-frequency-data.csv"),
+          githubStatsCodeFrequenciesAsCsv(yieldStatsCodeFrequency(gh, { maxDays: dataTimeframe, signal })),
+          { header: githubStatsCodeFrequencyHeaders },
+        )
+      })
+    }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("stats-commit-activity", async () => {
+        await writeCSVToFile(
+          join(outputDir, "github-stats-commit-activity-data.csv"),
+          githubStatsCommitActivityAsCsv(yieldStatsCommitActivity(gh, { maxDays: dataTimeframe, signal })),
+          { header: githubStatsCommitActivityHeaders },
+        )
+      })
+    }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("stats-contributors", async () => {
+        await writeCSVToFile(
+          join(outputDir, "github-stats-contributors-data.csv"),
+          githubStatsContributorsAsCsv(yieldStatsContributors(gh, { maxDays: dataTimeframe, signal })),
+          { header: githubStatsContributorsHeaders },
+        )
+      })
+    }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("stats-participation", async () => {
+        await writeCSVToFile(
+          join(outputDir, "github-stats-participation-data.csv"),
+          githubStatsParticipationAsCsv(yieldStatsParticipation(gh, { signal })),
+          { header: githubStatsParticipationHeaders },
+        )
+      })
+    }))
+
+    jobs.push(limit(async () => {
+      await timeCtx("stats-punch-card", async () => {
+        await writeCSVToFile(
+          join(outputDir, "github-stats-punch-card-data.csv"),
+          githubStatsPunchCardAsCsv(yieldStatsPunchCard(gh, { signal })),
+          { header: githubStatsPunchCardHeaders },
+        )
+      })
+    }))
+
+    for (const { mode, maxDays } of yieldHistogramTimeframes()) {
+      jobs.push(limit(async () => {
+        await timeCtx(`${mode} histograms`, async () => {
+          await writeCSVToFile(
+            join(outputDir, `pull-request-lead-times-${mode}.csv`),
+            pullRequestHistogramAsCsv(
+              inspectIter(
+                () => dot(),
+                yieldPullRequestHistogram(gh, {
+                  mode,
+                  maxDays,
+                  excludeLabels: github.pulls?.ignoreLabels,
+                  signal,
+                }),
+              ),
+            ),
+            { header: pullRequestHistogramHeaders.slice() },
+          )
+        })
+      }))
+    }
   }
+
+  queueGitHubReportJobs()
 
   try {
     await Promise.all(jobs)
