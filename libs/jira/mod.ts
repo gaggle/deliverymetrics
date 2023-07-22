46a9126402e442881459d5d8948c7b862c283e3f
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-22 11:50:07 +0200

    Expose jiraSyncInfoSchema in jira/mod.ts

--- libs/jira/mod.ts
+++ libs/jira/mod.ts
@@ -1,4 +1,5 @@
 export * from "./create-fake-jira-client.ts"
 export * from "./get-fake-jira-sync-info.ts"
+export * from "./jira-sync-info-schema.ts"
 export * from "./types.ts"
 export { getJiraClient } from "./jira-client.ts"
