0a40dbf8327b5cba1c5b29ed2df4bb751499e4a2
Jon Lauridsen <mail@jonlauridsen.com>
2023-07-27 23:50:56 +0200

    Add libs/version.ts

---
+++ libs/version.ts
@@ -0,0 +1,19 @@
+import * as path from "std:path"
+
+import { extractSemVers, single } from "./utils/mod.ts"
+
+const dirname = path.dirname(new URL(import.meta.url).pathname)
+
+async function extractReadmeVersion() {
+  const readmePath = path.join(dirname, "..", "README.md")
+  const content = await Deno.readTextFile(readmePath)
+  for (const line of content.split("\n")) {
+    if (line.includes("# gaggle/deliverymetrics")) {
+      return single(extractSemVers(line)).raw
+    }
+  }
+}
+
+export async function version() {
+  return await extractReadmeVersion() || "0.0.0+dev"
+}
