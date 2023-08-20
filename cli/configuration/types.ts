e4f2385954b07ed18d648424056766aeb8f4ba7d
Jon Lauridsen <mail@jonlauridsen.com>
2023-08-21 00:25:24 +0200

    Add dev_lead_time_statuses & dev_lead_time_types Jira report configuration fields

--- cli/configuration/types.ts
+++ cli/configuration/types.ts
@@ -64,6 +64,8 @@ const configReportSchema = z.object({
   jira: z.object({
     custom_completed_date_header: z.string().optional(),
     custom_start_date_header: z.string().optional(),
+    dev_lead_time_statuses: z.array(z.string()).optional(),
+    dev_lead_time_types: z.array(z.string()).optional(),
   })
     .optional(),
   outdir: z.string(),
