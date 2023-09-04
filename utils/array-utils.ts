0388b321ce667e9164466c9d994a3bf9b4612b47
Jon Lauridsen <mail@jonlauridsen.com>
2023-09-04 23:23:12 +0200

    Export regexOrStringTestMany and regexOrStringTest utility functions

--- utils/array-utils.ts
+++ utils/array-utils.ts
@@ -8,18 +8,23 @@ export function arraySubtract<T, U extends T>(
 }
 
 export function arraySubtractRegEx(arr: Array<string>, toSubtract: Array<string | RegExp>): Array<string> {
-  return arr.filter((el: string) => {
-    for (const sub of toSubtract) {
-      if (sub instanceof RegExp) {
-        if (sub.test(el)) {
-          return false
-        }
-      } else if (el === sub) {
-        return false
-      }
+  return arr.filter((el: string) => !regexOrStringTestMany(el, toSubtract))
+}
+
+export function regexOrStringTestMany(el: string, matches: Array<string | RegExp>): boolean {
+  for (const match of matches) {
+    if (regexOrStringTest(el, match)) {
+      return true
     }
-    return true
-  })
+  }
+  return false
+}
+
+export function regexOrStringTest(el: string, match: string | RegExp): boolean {
+  if (match instanceof RegExp) {
+    return match.test(el)
+  }
+  return el === match
 }
 
 export function average(nums: number[]): number {
