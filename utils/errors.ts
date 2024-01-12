67afe5266161ecadabbd05a61519b7fed34cc6b8
Jon Lauridsen <mail@jonlauridsen.com>
2024-01-12 19:10:34 +0100

    Add FetchError class to utils/errors.ts
    A new class FetchError has been added to handle errors during fetching. It extends the Error class and takes in an Error object, a Request object, and an optional Response object as arguments. The aim is to provide detailed error instances including request and response data, hence improving the debugging process.

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,1 +1,14 @@
-export class AbortError extends Error {}
+export class AbortError extends Error {
+}
+
+export class FetchError extends Error {
+  error: Error
+  request: Request
+  response?: Response
+  constructor(error: Error, request: Request, response?: Response) {
+    super(`FetchError due to: ${error}`)
+    this.error = error
+    this.request = request
+    this.response = response
+  }
+}
